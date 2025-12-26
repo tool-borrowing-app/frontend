a03d8d6da357bbb5296570bd337c5c289f2bf41e
Daniel Spaits <daniel.spaits@oracle.com>
2025-12-26 20:15:49 +0100

    show conversations

--- src/apiClient/types/conversation.types.ts
+++ src/apiClient/types/conversation.types.ts
@@ -1,3 +1,12 @@
+import { ToolDto, UserDto } from "@/app/eszkozeim/page";
+
 export type StartConversationPayload = {
   toolId: number;
+}
+
+export type ConversationDto = {
+  id: number;
+  tool: ToolDto;
+  renter: UserDto;
+  lender: UserDto;
 }
--- src/app/uzenetek/page.tsx
+++ src/app/uzenetek/page.tsx
@@ -1,19 +1,59 @@
 "use client";
 
 import { fetchConversations } from "@/apiClient/modules/conversation";
-import { useEffect } from "react";
+import { ConversationDto } from "@/apiClient/types/conversation.types";
+import { Button, Card, Container, Group, Stack, Image, Text } from "@mantine/core";
+import { useEffect, useState } from "react";
 
 export default function Page() {
+
+  const [allConversations, setAllConversations] = useState<ConversationDto[]>([]);
 
   useEffect(() => {
     getAllConversations();
   }, []);
 
   const getAllConversations = async() => {
-    await fetchConversations();
+    const res = await fetchConversations();
+    setAllConversations(res.data);
   }
 
   return (
-    <>üzenetek page</>
+    <>üzenetek page
+    <Button onClick={() => {console.log("all conversations: ", allConversations)}}>
+      Debug
+    </Button>
+      
+      <Container size={`sm`}>
+        <Stack gap={`sm`}>
+          {allConversations.map((conversation) => {
+            const tool = conversation.tool;
+            const owner = tool.user;
+            const imageUrl = tool.imageUrls?.[0];
+
+            return (
+              <Card key={conversation.id} withBorder radius="md">
+                <Group align="center" wrap="nowrap">
+                  <Image
+                     src={imageUrl}
+                      alt={tool.name}
+                      w={80}
+                      h={80}
+                      radius="md"
+                  ></Image>
+                  <Stack gap={2}>
+                  <Text fw={500}>{tool.user?.firstName} {tool.user?.lastName}</Text>
+                  <Text size="sm" c="dimmed">
+                    {tool.name}
+                  </Text>
+                </Stack>
+                </Group>
+              </Card>
+            );
+          })}
+        </Stack>
+      </Container>
+    </>
+  
   );
 }
